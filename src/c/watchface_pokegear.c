fe1645fa01dbaeef95772543e0f98a4b723e04f9
Imanolea <imanoleas@gmail.com>
2017-06-01 22:17:09 +0200

    Game & Watch background

--- src/c/watchface_pokegear.c
+++ src/c/watchface_pokegear.c
@@ -2,22 +2,37 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void main_window_load(Window *window) {
     // Obtenemos información acerca de la ventana
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
 
+    // Creamos un GBitmap
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+    // Creamos un BitmapLayer para mostrar el GBitmap
+    s_background_layer = bitmap_layer_create(bounds);
+
+    // Seteamos el bitmap en la capa y los añadimos a la ventana
+    bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
     // Creamos el TextLayer con las dimensiones específicas
     s_time_layer = text_layer_create(
-        GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+        GRect(67, 3, bounds.size.w, 50));
     
     // Mejoramos la alineación para que se parezca más a la de un watchface
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
     text_layer_set_text(s_time_layer, "00:00");
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-    text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+    // Creación de la fuente
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_25));
+    text_layer_set_font(s_time_layer, s_time_font);
+    text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
 
     // Se añade como capa hija a la capa raíz de la Window
     layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
@@ -26,13 +41,19 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
     // Destruimos TextLayer
     text_layer_destroy(s_time_layer);
+    // Descargamos la GFont
+    fonts_unload_custom_font(s_time_font);
+    // Destruimos el GBitmap
+    gbitmap_destroy(s_background_bitmap);
+    // Destruimos el BitmapLayer
+    bitmap_layer_destroy(s_background_layer);
 }
 
 static void update_time(struct tm *tick_time) {
 
     // Escribimos la hora minutos actuales en un buffer
     static char s_buffer[8];
-    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M":"%I:%M", tick_time);
 
     // Mostramos esta hora en el TextLayer
     text_layer_set_text(s_time_layer, s_buffer);
@@ -61,6 +82,8 @@ static void init() {
     // Nos aseguramos de que la hora se muetra desde el principio
     time_t temp = time(NULL);
     update_time(localtime(&temp));
+    // Fondo de color blanco
+    window_set_background_color(s_main_window, GColorWhite);
 }
 
 static void deinit() {
